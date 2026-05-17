bf6860ee8e87ce8eb928791b9f907a5cc5957997
VitorGomesss <163527931+VitorGomesss@users.noreply.github.com>
2026-05-17 16:21:37 -0300

    Feat: inicialize a página de destino com um sistema de design inspirado na era cibernética e uma seção principal responsiva.

---
+++ script.js
@@ -0,0 +1,4 @@
+// Aqui futuramente entrarão as lógicas de interatividade (menu mobile, animações de scroll, etc)
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("CoreTec - Scripts carregados.");
+});
